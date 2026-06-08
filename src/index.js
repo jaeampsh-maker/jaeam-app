5f4f678a773d6e6e6971062a34df2eb9ad1c9972
jaeampsh-maker <jaeam.psh@gmail.com>
2026-06-08 17:30:59 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-// src/index.js - PWA 서비스워커 등록 포함
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
@@ -11,5 +10,4 @@ root.render(
   </React.StrictMode>
 );
 
-// PWA 서비스워커 등록 (오프라인 지원)
 register();
